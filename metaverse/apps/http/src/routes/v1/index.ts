4515ab03041f6225ce665f395ec72afa794c9e3b
kylanalicia <aliciakimani.ky@gmail.com>
2025-04-09 23:20:36 +0300

    setup the database

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 export const router = Router();
 
