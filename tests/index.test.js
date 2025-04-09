d82422a9c94456c524f4017ad7627c63dbfcffc3
kylanalicia <aliciakimani.ky@gmail.com>
2025-04-09 08:00:23 +0300

    Created tests for available avatars and other users metadata(name & avatarUrl)

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,75 +6,75 @@ function sum(a,b) {
 
 const BACKEND_URL = "http://localhost:3000"
 
-// describe("Authentication", () => {
-//     test('User is able to sign up only once', async () => {
-//         const username = "kirat" + Math.random(); // kirat0.12331313
-//         const password = "123456";
-//         const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
-//             username,
-//             password,
-//             type: "admin"
-//         })
+describe("Authentication", () => {
+    test('User is able to sign up only once', async () => {
+        const username = "kirat" + Math.random(); // kirat0.12331313
+        const password = "123456";
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        })
 
-//         expect(response.status).toBe(200)
-//         const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
-//             username,
-//             password,
-//             type: "admin"
-//         })
+        expect(response.status).toBe(200)
+        const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        })
 
-//         expect(updatedResponse.status).toBe(400);
-//     });
+        expect(updatedResponse.status).toBe(400);
+    });
 
-//     test('Signup request fails if the username is empty', async () => {
-//         const username = `kirat-${Math.random()}` // kirat-0.12312313
-//         const password = "123456"
+    test('Signup request fails if the username is empty', async () => {
+        const username = `kirat-${Math.random()}` // kirat-0.12312313
+        const password = "123456"
 
-//         const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
-//             password
-//         })
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            password
+        })
 
-//         expect(response.status).toBe(400)
-//     })
+        expect(response.status).toBe(400)
+    })
 
-//     test('Signin succeeds if the username and password are correct', async() => {
-//         const username = `kirat-${Math.random()}`
-//         const password = "123456"
+    test('Signin succeeds if the username and password are correct', async() => {
+        const username = `kirat-${Math.random()}`
+        const password = "123456"
 
-//         await axios.post(`${BACKEND_URL}/api/v1/signup`, {
-//             username,
-//             password,
-//             type: "admin"
-//         });
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        });
 
-//         const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
-//             username,
-//             password
-//         });
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username,
+            password
+        });
 
-//         expect(response.status).toBe(200)
-//         expect(response.data.token).toBeDefined()
+        expect(response.status).toBe(200)
+        expect(response.data.token).toBeDefined()
         
-//     })
+    })
 
-//     test('Signin fails if the username and password are incorrect', async() => {
-//         const username = `kirat-${Math.random()}`
-//         const password = "123456"
+    test('Signin fails if the username and password are incorrect', async() => {
+        const username = `kirat-${Math.random()}`
+        const password = "123456"
 
-//         await axios.post(`${BACKEND_URL}/api/v1/signup`, {
-//             username,
-//             password,
-//             role: "admin"
-//         });
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            role: "admin"
+        });
 
-//         const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
-//             username: "WrongUsername",
-//             password
-//         })
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username: "WrongUsername",
+            password
+        })
 
-//         expect(response.status).toBe(403)
-//     })
-// })
+        expect(response.status).toBe(403)
+    })
+})
 
 describe("User metadata endpoint", () => {
     let token = "";
@@ -138,3 +138,50 @@ describe("User metadata endpoint", () => {
     test("test 3", () => {
     })
 })
+
+describe("User avatar information", () => {
+    let avatarId;
+    let token;
+    let userId;
+
+    beforeAll(async() => {
+        const username = `kirat-${Math.random()}`
+        const password = '123456'
+
+        const signupResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        })
+
+        userId = signupResponse.data.userId
+
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username,
+            password
+        })
+
+        token = response.data.token
+
+        const AvatarResponse = await axios.post(`${{BACKEND_URL}/api/v1/avatar}`, {
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+            "name": "Timmy"
+        })
+        avatarId = AvatarResponse.data.avatarId;
+    })
+    test("Get back avatar information for a user", async () => {
+        const response = await axios.get(`${BACKEND_URL}/api/v1/user/metadata/bulk?ids=[${userId}]`);
+        expect(response.data.avatars.length).toBe(1);
+        expect(response.data.avatars[0].userId).toBe(userId);
+    })
+
+    test("Available avatars lists the recently created avatar", async () => {
+        const response = await axios.get(`${BACKEND_URL}/api/v1/avatars`);
+        expect(response.data.avatars.length).not.toBe(0);
+        const currentAvatar = response.data.avatars.find(x => x.id == avatarId);
+        expect(currentAvatar).toBeDefined()
+    })
+
+    
+    })
+    
