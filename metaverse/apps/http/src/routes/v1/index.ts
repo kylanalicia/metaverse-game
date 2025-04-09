7b2e27ce7fa6d3af960fd930bf43c500a9b91aa0
kylanalicia <aliciakimani.ky@gmail.com>
2025-04-09 20:40:11 +0300

    added more information in some files and installed prisma

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -22,3 +22,8 @@ router.get("/elements", (req, res) => {
 router.get("/avatars", (req, res) => {
     
 })
+
+router.use("/user", userRouter)
+router.use("/space", spaceRouter)
+router.use("/admin", adminRouter)
+
